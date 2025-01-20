c3f46a74ced835f2ea720827c25d935b855a217b
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-01-20 11:02:35 +0000

    Create setup for basic arithmetic operations
    Create functions for the basic arithmetic operations.
    Create a function to check the type of operand.

---
+++ script.js
@@ -0,0 +1,33 @@
+/*
+Variables for operations:
+first number, second number, operand
+
+operations to run:
+add, subtract, multiply, divide
+
+*/
+
+const add = '+', subtract = '-', multiply = 'x', divide = '/';
+
+function addition(num1, num2) {return num1 + num2;}
+
+function subtraction(num1, num2) {return num1 - num2;}
+
+function multiplication(num1, num2) {return num1 * num2;}
+
+function division(num1, num2) {return num2 === 0 ? 'Math Error: Division by zero' : num1 / num2;}
+
+function operate(num1, operand, num2) {
+    switch (operand) {
+        case add:
+            return addition(num1, num2);
+        case subtract:
+            return subtraction(num1, num2);
+        case multiply:
+            return multiplication(num1, num2);
+        case divide:
+            return division(num1, num2);
+    }
+}
+
+console.log(operate(2, '/', 0));
