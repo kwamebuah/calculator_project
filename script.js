99720f016c2b5c5f7fba6f08487494874239aa12
kwamebuah <66295784+kwamebuah@users.noreply.github.com>
2025-01-21 12:46:40 +0000

    Provide functionality for displaying numbers
    Display numbers, including decimals, on the display as user enters.

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 // collection of keys
 const allButtons = [
-    'AC','+/-','%','÷',
-    '7','8','9','×',
-    '4','5','6','-',
-    '1','2','3','+',
-    'OT','0','.','='
+    'AC', '+/-', '%', '÷',
+    '7', '8', '9', '×',
+    '4', '5', '6', '-',
+    '1', '2', '3', '+',
+    'OT', '0', '.', '='
 ];
 
-const operationButtons = ['÷','×','-','+','='];
-const topButtons = ['AC','+/-','%'];
+let A = 0;
+let operation = null;
+let B = null;
+
+const operationButtons = ['÷', '×', '-', '+', '='];
+const topButtons = ['AC', '+/-', '%'];
 
 const buttonArea = document.querySelector('.keys');
+const display = document.querySelector('.display');
+
+display.value = '0';
 
 for (let i = 0; i < allButtons.length; i++) {
     let value = allButtons[i];
@@ -29,6 +36,36 @@ for (let i = 0; i < allButtons.length; i++) {
         button.style.backgroundColor = '#656565';
         button.style.color = '#fff';
     }
-    
+
+    button.addEventListener('click', () => {
+        if (operationButtons.includes(value)) {
+
+        }
+        else if (topButtons.includes(value)) {
+
+        }
+        else {
+            if (value === '.') {
+
+                if (!display.value.includes(value)) {
+
+                    display.value += value;
+                }
+            }
+            else if (value === 'OT') {
+
+            }
+            else {
+                if ((display.value[0] === '0') && (display.value[1] !== '.')) {
+                    display.value = '';
+                    display.value += value;
+                }
+                else {
+                    display.value += value;
+                }
+            }
+        }
+    });
+
     buttonArea.appendChild(button);
 }
